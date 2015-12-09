bacc82c1439f4e4b628ab10a7c474b55d890be3e
Justin Livi <justin@brinkbit.com>
2015-12-09 16:44:51 -0500

    Remove downstream changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,16 +29,6 @@ module.exports = function initGrunt( grunt ) {
                         cwd: 'src/',
                         src: [ '**' ],
                         dest: 'build/'
-                    },
-                    {
-                        expand: true,
-                        cwd: 'node_modules/phaser/build/',
-                        src: [ 'phaser.map', 'phaser.min.js' ],
-                        dest: 'build/'
-                    },
-                    {
-                        src: 'node_modules/store/store.min.js',
-                        dest: 'build/store.min.js'
                     }
                 ]
             }
